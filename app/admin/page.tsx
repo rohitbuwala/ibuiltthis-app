9a12ea7e4516de66b91e5c0a51636dd41336caab
rohit buwala <rohitbuwala821gmail.com>
2025-12-31 17:30:42 +0530

    feat: implement admin layout component with suspense for loading state

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,81 +1,85 @@
-
 import AdminProductCard from "@/components/admin/admin-product-card";
 import StatsCard from "@/components/admin/stats-card";
 import EmptyState from "@/components/common/empty-state";
 import SectionHeader from "@/components/common/section-header";
-import ProductCard from "@/components/products/product-card";
-import { getAllApprovedProducts, getAllProducts } from "@/lib/products/product-select";
-import { auth, clerkClient } from "@clerk/nextjs/server"
-import { Inbox, InboxIcon, ShieldIcon } from "lucide-react";
+import { getAllProducts } from "@/lib/products/product-select";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { InboxIcon, ShieldIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
-
 export default async function AdminPage() {
-    const {userId} = await auth();
+  const { userId } = await auth();
 
-    if(!userId){
-        redirect("/sign-in")
-    }
+  if (!userId) {
+    redirect("/sign-in");
+  }
 
-    const respone = await clerkClient();
-    const user = await respone.users.getUser(userId!);
-    
-    const metadata = user.publicMetadata;
-    const isAdmin = metadata.isAdmin ?? false;
+  const response = await clerkClient();
+  const user = await response.users.getUser(userId!);
 
-    if(!isAdmin){
-        redirect("/")
-    }
+  const metadata = user.publicMetadata;
+  const isAdmin = metadata?.isAdmin ?? false;
 
-    const allProducts = await getAllProducts();
-    const approvedProducts = allProducts.filter((product) => product.status === "approved")
-
-    const pendingProducts = allProducts.filter((product) => product.status === "pending");
-
-    const rejectedProducts = allProducts.filter((product) => product.status === "rejected");
-
+  if (!isAdmin) {
+    redirect("/");
+  }
+  const allProducts = await getAllProducts();
+  const approvedProducts = allProducts.filter(
+    (product) => product.status === "approved"
+  );
+  const pendingProducts = allProducts.filter(
+    (product) => product.status === "pending"
+  );
+  const rejectedProducts = allProducts.filter(
+    (product) => product.status === "rejected"
+  );
   return (
     <div className="py-20">
-        <div className="wrapper">
-         <div className="mb-12">
-         <SectionHeader
+      <div className="wrapper">
+        <div className="mb-12">
+          <SectionHeader
             title="Product Admin"
             icon={ShieldIcon}
             description="Review and manage submitted products"
-            />
+          />
+        </div>
+        <StatsCard
+          approved={approvedProducts.length}
+          pending={pendingProducts.length}
+          rejected={rejectedProducts.length}
+          all={allProducts.length}
+        />
+
+        <section className="my-12">
+          <div className="section-header-with-count">
+            <h2 className="text-2xl font-bold">
+              Pending Products ({pendingProducts.length})
+            </h2>
           </div>
-       <StatsCard
-            approved={approvedProducts.length}
-            pending={pendingProducts.length}
-            rejected={rejectedProducts.length}
-            all={allProducts.length}
-       />
-       <section className="my-12">
-        <div className="section-header-with-count">
-            <h2 className="text-2xl font-bold">Pending Product ({pendingProducts.length})</h2>
-        </div>
-        <div className="space-y-4">
-       {pendingProducts.length === 0 && (
-        <EmptyState message="No pending product to review"
-        icon={InboxIcon}/>
-       )} {pendingProducts.map((product) => (
+          <div className="space-y-4">
+            {pendingProducts.length === 0 && (
+              <EmptyState
+                message="No pending products to review"
+                icon={InboxIcon}
+              />
+            )}
+            {pendingProducts.map((product) => (
               <AdminProductCard key={product.id} product={product} />
             ))}
-        </div>
-       </section>
+          </div>
+        </section>
 
-       <section className="my-12">
-        <div className="section-header-with-count">
-            <h2 className="text-2xl font-bold">All Product ({pendingProducts.length})</h2>
-        </div>
-        <div className="space-y-4">
-       {allProducts.map((product) => (
+        <section className="my-12">
+          <div className="section-header-with-count">
+            <h2 className="text-2xl font-bold">All Products</h2>
+          </div>
+          <div className="space-y-4">
+            {allProducts.map((product) => (
               <AdminProductCard key={product.id} product={product} />
             ))}
-        </div>
-       </section>
+          </div>
+        </section>
+      </div>
     </div>
- </div>
-  )
+  );
 }
-
